4006b822cd15a9c6b071e0ce73900356cf5f8f58
Sebastian Whyte <whyte.sebastian14@gmail.com>
2023-06-09 18:21:14 -0400

    Got the box to take up the full size of the screen, imeplemented onClick function each menu button, and created custom test alerts that trigger when the onClick function occurs

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,10 @@
 import React from 'react';
 import './App.css';
 import {Container} from "@mui/system";
-import Grid from "@mui/material/Grid"
 import Box from "@mui/material/Box";
 import Header from './components/Header'
 import ButtonContainer from './components/Buttons';
 import Logo from './components/Logo'
-
-
-{/*
-function App() 
-{
-  return (
-    <div className="App">
-      <div className='backdrop'>
-         <div className='logo'>
-            <img src={logo} height='200' width='200'></img>
-         </div>
-         <div className='header'>
-            <Header message={'Game Deals App'} />
-         </div>
-         <div className='button-container'>
-            <ButtonContainer />
-         </div>
-      </div>
-    </div>
-  );
-}
-
-export default App;
-*/}
 
 
 export default function App()
@@ -37,7 +12,7 @@ export default function App()
    return (
       <div className='App'>
          <Container maxWidth="xl" sx={{marginTop:0}} >
-            <Box justifyContent='center' alignItems='center' minHeight='100vh' style={{ color: 'white', backgroundColor: '#174cfc' }}>
+            <Box justifyContent='center' alignItems='center' minHeight='100vh' minWidth='100vw' style={{ color: 'white', backgroundColor: '#174cfc' }}>
                   <Logo />
                   <Header message={'Game Deals App'}/>
                   <ButtonContainer />
--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,21 +1,31 @@
+import React, {Component,} from "react";
 import { ButtonGroup } from "@mui/material";
 import Button from "@mui/material/Button";
-import Stack from "@mui/material/Stack";
-import { useState } from "react";
+import { Route } from "react-router-dom";
 
 
 const buttons = [
-    <Button key='view-stores'>View Stores</Button>,
-    <Button key='list-all-deals'>List All Deals</Button>,
-    <Button key='game-lookup'>Game Lookup By Title</Button>
+    <Button key='view-stores' onClick={() => displayStores()}>View Stores</Button>,
+    <Button key='list-all-deals' onClick={() => displayAllDeals()}>List All Deals</Button>,
+    <Button key='game-lookup' onClick={() => displayGameLookup()}>Game Lookup By Title</Button>
 ];
 
-function handleClick()
+
+function displayStores()
 {
-    return (
-        alert('You clicked me')
-    );
+    alert('Stores: Steam, Gamers Gate, Green Man Gaming, Amazon, GameStop');
 }
+
+function displayAllDeals()
+{
+    alert('List All Deals');
+}
+
+function displayGameLookup()
+{
+    alert('Game Lookup By Title');
+}
+
 
 export default function ButtonContainer()
 {
@@ -25,7 +35,8 @@ export default function ButtonContainer()
                 orientation="vertical"
                 aria-label="vertical contained button group"
                 variant="contained" 
-                onClick={handleClick}>{buttons}
+                >
+                  {buttons}
                 </ButtonGroup>
         </div>
     )
